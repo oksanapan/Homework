ee433f54d8f9ea1e125b0f6ca09b7cb4aefcb137
oksana <oksanapan230478@gmail.com>
2023-07-05 13:11:35 +0300

    домашня1

--- src/homework1/Task4.java
+++ src/homework1/Task4.java
@@ -1,4 +1,13 @@
 package homework1;
 
+
 public class Task4 {
+    public static void main(String[] args) {
+int a = 7;
+int b = 9;
+a += 2;
+b += - 2;
+System.out.println(a);
+System.out.println(b);
+    }
 }
