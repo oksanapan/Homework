3d7a151e02966bef0bf7a57446efb4c3a88f978c
oksana <oksanapan230478@gmail.com>
2023-07-07 12:49:55 +0300

    домашня2завдання2

--- src/homework2/Task1.java
+++ src/homework2/Task1.java
@@ -3,8 +3,8 @@ package homework2;
 public class Task1 {
     public static void main(String[] args) {
         String String1 = "This line that i want to cut, cause it is too; long";
-        String String2 = String1.substring(0,32);
-        String String3 = String2 + "  don’t " +;
+        String String2 = String1.substring(0,35);
+        String String3 = String2 + "  don’t " ;
 
         System.out.println(String1);
         System.out.println(String2);
---
+++ src/homework2/Task3.java
@@ -0,0 +1,7 @@
+package homework2;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+}
