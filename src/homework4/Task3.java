7f6ed38cf8881434f26c8bbfa3ad0d44b2b95dde
oksana <oksanapan230478@gmail.com>
2023-07-15 14:03:55 +0300

    домашня 4 завдання3

--- src/homework4/Task3.java
+++ src/homework4/Task3.java
@@ -1,7 +1,31 @@
 package homework4;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть вмістимість складу (вагу):");
+        int capacity = scanner.nextInt();
+        int remainingCapacity = capacity;
+        int metalWeight = 0;
+        while (remainingCapacity > 0) {
+            System.out.print("Введіть вагуетала для здачі:");
+            metalWeight = scanner.nextInt();
+            if (metalWeight < 5) {
+                System.out.println("Неможливо прийняти метал вагою менше 5");
+                continue;
+            }
+            if (metalWeight > remainingCapacity) ;
+            System.out.println("Неможливо прийняти метал.Не достатньовільного місця на складі");
+            break;
+        }
+        remainingCapacity -= metalWeight;
+        System.out.println("Прийнято" + metalWeight + " кг метала.Остача вільного місця на складі: " + remainingCapacity + "кг");
+        System.out.println("Місце на складі закінчилося. Програма завершина");
 
     }
-}
+        }
+
+
+
